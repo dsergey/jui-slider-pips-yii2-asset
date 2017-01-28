0fa45de494d23e6506d7ea3f2e4b72e389a6c488
sergey <serik-drozd@mail.ru>
2017-01-28 08:21:20 +0200

    fixes

--- JuiSliderPipsAsset.php
+++ JuiSliderPipsAsset.php
@@ -6,16 +6,27 @@ namespace dsergey\assets;
 
 use yii\web\AssetBundle;
 
+/**
+ * @author Sergey Drozd <serik.drozd@gmail.com>
+ */
 class JuiSliderPipsAsset extends AssetBundle
 {
-    public $sourcePath = './jQuery-ui-Slider-Pips/dist/';
     public $js = [
-         'jquery-ui-slider-pips.min.js',
+        'jquery-ui-slider-pips.min.js',
     ];
     public $css = [
         'jquery-ui-slider-pips.min.css',
     ];
     public $depends = [
-        'yii\web\JuiAsset',
+        'yii\jui\JuiAsset',
     ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->sourcePath = __DIR__ . '/jQuery-ui-Slider-Pips/dist/';
+        parent::init();
+    }
 }
